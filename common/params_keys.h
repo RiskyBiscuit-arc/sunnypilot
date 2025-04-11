e2ff86140a01951287fec8282a501ec25fbdc96a
rav4kumar <meetkumardesai@gmail.com>
2025-04-10 22:48:31 -0700

    abh, bsm

--- common/params_keys.h
+++ common/params_keys.h
@@ -167,4 +167,9 @@ inline static std::unordered_map<std::string, uint32_t> keys = {
     {"HyundaiLongitudinalTuning", PERSISTENT},
 
     {"DynamicExperimentalControl", PERSISTENT},
+
+    // toyota specific params
+    {"ToyotaAutoHold", PERSISTENT},
+    {"ToyotaEnhancedBsm", PERSISTENT},
+    {"ToyotaTSS2Long", PERSISTENT},
 };
